f38e316b045d0ec472e3435349e806822afa01b8
jglrxavpok <jglrxavpok@gmail.com>
2015-03-08 18:12:10 +0100

    Added LWJGL natives helpers

--- src/main/java/org/c1/C1Game.java
+++ src/main/java/org/c1/C1Game.java
@@ -1,31 +1,114 @@
 package org.c1;
 
-import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
+import static org.lwjgl.opengl.GL11.*;
+
+import java.io.*;
+
+import org.lwjgl.*;
+import org.lwjgl.opengl.*;
+import org.slf4j.*;
 
 public class C1Game {
 
-	public void start() {
-		try {
-			Display.setDisplayMode(new DisplayMode(800, 600));
-			Display.create();
-		} catch (LWJGLException e) {
-			e.printStackTrace();
-			System.exit(0);
-		}
+    private int fps;
+    private boolean running;
+    private Logger logger;
+    private File gameFolder;
 
-		while (!Display.isCloseRequested()) {
+    public void start() {
+        try {
+            float ratio = 16f / 9f;
+            int width = 940;
+            LWJGLSetup.load(new File(getGameFolder(), "natives"));
+            Display.setDisplayMode(new DisplayMode(width, (int) (width / ratio)));
+            Display.create();
+        } catch (LWJGLException | IOException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
 
-			Display.update();
-		}
+        logger = LoggerFactory.getLogger("0xC1");
+        long lastTime = System.nanoTime();
+        double delta = 0.0;
+        double ns = 1_000_000_000.0 / 60.0;
+        long timer = System.currentTimeMillis();
+        int updates = 0;
+        int frames = 0;
+        running = true;
+        while (running) // Thanks to TheCherno for the code of this loop,
+                        // check him out on GitHub, he does a lot of cool
+                        // stuff
+        {
+            long now = System.nanoTime();
+            boolean polledInput = false;
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            if (delta >= 1.0) {
+                if (!polledInput) {
+                    pollEvents();
+                    polledInput = true;
+                }
+                double deltaTime = ns / 1_000_000_000.0;
+                update(deltaTime);
+                updates++;
+                delta--;
+            }
+            render(ns / 1_000_000_000.0);
+            Display.update();
+            frames++;
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                logger.trace(updates + " ups, " + frames + " fps");
+                Display.setTitle("0xC1 - " + fps + " fps");
+                fps = frames;
+                updates = 0;
+                frames = 0;
+            }
 
-		Display.destroy();
-	}
+            if (Display.isCloseRequested())
+                running = true;
+        }
 
-	public static void main(String[] args) {
-		C1Game game = new C1Game();
-		game.start();
-	}
+        Display.destroy();
+    }
+
+    private void pollEvents() {
+        // TODO Implement
+
+    }
+
+    private void update(double deltaTime) {
+        // TODO Implement
+
+    }
+
+    private void render(double deltaTime) {
+        // TODO Implement
+        glClearColor(0, 0, 0, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glClear(GL_DEPTH_BUFFER_BIT);
+
+        glColor4f(1, 1, 0, 1);
+        glRectf(0f, 0f, 1f, 1f);
+    }
+
+    public File getGameFolder() {
+        if (gameFolder == null) {
+            String appdata = System.getenv("APPDATA");
+            if (appdata == null)
+                gameFolder = new File(System.getProperty("user.home"), "0xC1");
+            else
+                gameFolder = new File(appdata, "0xC1");
+            if (!gameFolder.exists())
+                gameFolder.mkdirs();
+        }
+        return gameFolder;
+    }
+
+    public static void main(String[] args) {
+        C1Game game = new C1Game();
+        game.start();
+    }
 
 }
---
+++ src/main/java/org/c1/utils/IOUtils.java
@@ -0,0 +1,18 @@
+package org.c1.utils;
+
+import java.io.*;
+
+public class IOUtils {
+
+    public static void copy(InputStream in, OutputStream out)
+            throws IOException {
+        byte[] buffer = new byte[4096];
+        int i;
+
+        while ((i = in.read(buffer)) != -1) {
+            out.write(buffer, 0, i);
+        }
+        out.flush();
+    }
+
+}
